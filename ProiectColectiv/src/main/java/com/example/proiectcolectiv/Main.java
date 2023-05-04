340efadc960c83996d599ca6df64394f9a52fa4e
Lazar-Iasmina <107914315+Lazar-Iasmina@users.noreply.github.com>
2023-05-04 20:08:54 +0300

    Delete ProiectColectiv directory

--- ProiectColectiv/src/main/java/com/example/proiectcolectiv/Main.java
+++
@@ -1,9 +0,0 @@
-package com.example.proiectcolectiv;
-
-import javax.xml.crypto.Data;
-
-public class Main {
-    public static void main( String []args){
-       // DataBase.createTableUser("USER");
-    }
-}
