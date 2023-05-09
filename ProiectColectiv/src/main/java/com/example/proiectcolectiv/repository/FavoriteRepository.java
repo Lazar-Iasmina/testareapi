02fbc1d41fef1ff6255788e8f87bb6416414c886
Lazar-Iasmina <107914315+Lazar-Iasmina@users.noreply.github.com>
2023-05-09 10:25:29 +0300

    Delete ProiectColectiv directory

--- ProiectColectiv/src/main/java/com/example/proiectcolectiv/repository/FavoriteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.proiectcolectiv.repository;
-
-
-import com.example.proiectcolectiv.model.Favorite;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface FavoriteRepository extends JpaRepository<Favorite, Integer> {
-}
